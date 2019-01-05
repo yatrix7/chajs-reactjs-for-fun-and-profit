448aad449890033ce7b9c88df31d821435c621a1
Jamison Yates <jamisonyates@Jamisons-MacBook-Pro.local>
2019-01-05 00:38:10 -0500

    removed a kajillion const keywords

--- src/helpers/platesPerSideCalculator.js
+++ src/helpers/platesPerSideCalculator.js
@@ -7,16 +7,16 @@ const calculator = function(plates, weight) {
 	if (plates.length === 0 || weight < 0) return {}
 
 	// always make sure the plates are sorted so we don't add on nine 5's in stead of a single 45
-	const sortedPlates = plates.sort(reverse)
-	// grab the heaviest plate
-	const plateWeight = sortedPlates[0]
-	// how many of the heaviest plate can we use
-	const count = Math.floor(weight / plateWeight)
-	const totals = { [plateWeight.toString()]: count }
-	// slice off the plate we just used for *this* calulation
-	// as we don't want to calculate against it in the next iteration
-	const remaingPlates = sortedPlates.slice(1)
-	const remainingWeight = weight - count * plateWeight
+	const sortedPlates = plates.sort(reverse),
+		// grab the heaviest plate
+		plateWeight = sortedPlates[0],
+		// how many of the heaviest plate can we use
+		count = Math.floor(weight / plateWeight),
+		totals = { [plateWeight.toString()]: count },
+		// slice off the plate we just used for *this* calulation
+		// as we don't want to calculate against it in the next iteration
+		remaingPlates = sortedPlates.slice(1),
+		remainingWeight = weight - count * plateWeight
 
 	// recurse the remaining weight and plates
 	return Object.assign({}, calculator(remaingPlates, remainingWeight), totals)
